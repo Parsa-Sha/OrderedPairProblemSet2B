c3c0eb1935dad1afd928266162418b19ca3b073d
Parsa-Sha <99561759+Parsa-Sha@users.noreply.github.com>
2023-09-25 10:57:18 -0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class main {
+    public static void main(String[] args) {
+        OrderedPair pairObj = new OrderedPair(4, -5);
+        System.out.print(pairObj);
+    }
+}
